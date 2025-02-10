e9250fc6fc490445ff63fe146bccc061141fa6a5
martin <martintang410@gmail.com>
2025-02-09 16:35:47 -0800

    stuff

--- Crazy/Crazy.js
+++ Crazy/Crazy.js
@@ -1,66 +1,68 @@
 
-let Start = True
+
 
 function setup() {
-  createCanvas(400, 400);
-  frameRate(1);
-}
-function draw() {
-  background("yellowgreen");  
-    Draw_Flower()
-
-}
-
-function Draw_Flower() {
-  // Create flower object.
-  let myFlower = createFlower();
-
+    createCanvas(1300, 900);
+    frameRate(1);
+  }
+  function draw() {
+    background("yellowgreen");  
+      Draw_Flower()
+      Draw_Flower()
+      Draw_Flower()
   
-  // Use flower object properties to draw an ellipse.
-  fill(myFlower.color);
-  ellipse(myFlower.x, myFlower.y, myFlower.size);
-  fill(myFlower.color);
-  ellipse(myFlower.x + myFlower.size/2, myFlower.y + myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
-  fill(myFlower.color);
-  ellipse(myFlower.x - myFlower.size/2, myFlower.y - myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
-  fill(myFlower.color);
-  ellipse(myFlower.x + myFlower.size/2, myFlower.y - myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
-  fill(myFlower.color);
-  ellipse(myFlower.x - myFlower.size/2, myFlower.y + myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
-  fill(myFlower.color);
-  ellipse(myFlower.x, myFlower.y + myFlower.size - myFlower.size/16 * 5, myFlower.size - myFlower.size/8 * 5);
-  fill(myFlower.color);
-  ellipse(myFlower.x, myFlower.y - myFlower.size + myFlower.size/16 * 5, myFlower.size - myFlower.size/8 * 5);
-  fill(myFlower.color);
-  ellipse(myFlower.x - myFlower.size + myFlower.size/16 * 5, myFlower.y, myFlower.size - myFlower.size/8 * 5);
-  fill(myFlower.color);
-  ellipse(myFlower.x + myFlower.size - myFlower.size/16 * 5, myFlower.y, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x + myFlower.size/4 + myFlower.size/2 - myFlower.size/32 * 3, myFlower.y - myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x + myFlower.size/4 + myFlower.size/2 - myFlower.size/32 * 3, myFlower.y + myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x - myFlower.size/4 - myFlower.size/2 + myFlower.size/32 * 3, myFlower.y + myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x - myFlower.size/4 - myFlower.size/2 + myFlower.size/32 * 3, myFlower.y - myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x - myFlower.size/4 - myFlower.size/2 + myFlower.size/32 * 3, myFlower.y - myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x + myFlower.size/32 * 9, myFlower.y - myFlower.size + myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x - myFlower.size/32 * 9, myFlower.y - myFlower.size + myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x + myFlower.size/32 * 9, myFlower.y + myFlower.size - myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
-  fill(color(random(255), random(255), random(255)));
-  ellipse(myFlower.x - myFlower.size/32 * 9, myFlower.y + myFlower.size - myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
-}
-function createFlower() {
-  let flower = {
-    x: random(70,330),
-    y: random(70,330),
-    size: random(20,75),
-    lifespan: random(255,300),
-    color: color(random(255), random(255), random(255))
-  };
-  return flower;
-}
+  }
+  
+  function Draw_Flower() {
+    // Create flower object.
+    let myFlower = createFlower();
+  
+    
+    // Use flower object properties to draw an ellipse.
+    fill(myFlower.color);
+    ellipse(myFlower.x, myFlower.y, myFlower.size);
+    fill(myFlower.color);
+    ellipse(myFlower.x + myFlower.size/2, myFlower.y + myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
+    fill(myFlower.color);
+    ellipse(myFlower.x - myFlower.size/2, myFlower.y - myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
+    fill(myFlower.color);
+    ellipse(myFlower.x + myFlower.size/2, myFlower.y - myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
+    fill(myFlower.color);
+    ellipse(myFlower.x - myFlower.size/2, myFlower.y + myFlower.size/2, myFlower.size - myFlower.size/8 * 5);
+    fill(myFlower.color);
+    ellipse(myFlower.x, myFlower.y + myFlower.size - myFlower.size/16 * 5, myFlower.size - myFlower.size/8 * 5);
+    fill(myFlower.color);
+    ellipse(myFlower.x, myFlower.y - myFlower.size + myFlower.size/16 * 5, myFlower.size - myFlower.size/8 * 5);
+    fill(myFlower.color);
+    ellipse(myFlower.x - myFlower.size + myFlower.size/16 * 5, myFlower.y, myFlower.size - myFlower.size/8 * 5);
+    fill(myFlower.color);
+    ellipse(myFlower.x + myFlower.size - myFlower.size/16 * 5, myFlower.y, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x + myFlower.size/4 + myFlower.size/2 - myFlower.size/32 * 3, myFlower.y - myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x + myFlower.size/4 + myFlower.size/2 - myFlower.size/32 * 3, myFlower.y + myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x - myFlower.size/4 - myFlower.size/2 + myFlower.size/32 * 3, myFlower.y + myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x - myFlower.size/4 - myFlower.size/2 + myFlower.size/32 * 3, myFlower.y - myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x - myFlower.size/4 - myFlower.size/2 + myFlower.size/32 * 3, myFlower.y - myFlower.size/4, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x + myFlower.size/32 * 9, myFlower.y - myFlower.size + myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x - myFlower.size/32 * 9, myFlower.y - myFlower.size + myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x + myFlower.size/32 * 9, myFlower.y + myFlower.size - myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
+    fill(color(random(255), random(255), random(255)));
+    ellipse(myFlower.x - myFlower.size/32 * 9, myFlower.y + myFlower.size - myFlower.size/16 * 6, myFlower.size - myFlower.size/8 * 5);
+  }
+  function createFlower() {
+    let flower = {
+      x: random(80, 820),
+      y: random(80,820),
+      size: random(20,75),
+      lifespan: random(255,300),
+      color: color(random(255), random(255), random(255))
+    };
+    return flower;
+  }
