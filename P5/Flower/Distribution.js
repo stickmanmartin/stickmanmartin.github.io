cb177bc0ceb6710166d442d7d60599cf727328a3
martin <martintang410@gmail.com>
2025-03-03 16:38:44 -0800

    god stuff

--- P5/Flower/Distribution.js
+++ P5/Flower/Distribution.js
@@ -19,4 +19,24 @@ function draw() {
     noStroke();
     fill(0, 0, 255, 70);
     circle(a, b, 16);
+    let v = randomGaussian(400, 100);
+    let s = randomGaussian(400, 100);
+    noStroke();
+    fill(255, 255, 0, 70);
+    circle(v, s, 16);
+    let g = randomGaussian(400, 100);
+    let h = randomGaussian(400, 100);
+    noStroke();
+    fill(127, 0, 255, 70);
+    circle(g, h, 16);
+    let f = randomGaussian(400, 100);
+    let j = randomGaussian(400, 100);
+    noStroke();
+    fill(255, 255, 255, 70);
+    circle(f, j, 16);
+    let r = randomGaussian(400, 100);
+    let d = randomGaussian(400, 100);
+    noStroke();
+    fill(0, 0, 0, 70);
+    circle(r, d, 16);
 }
