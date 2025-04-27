71c291e0b54e81bdd14d63774696081fee5846d5
martin <martintang410@gmail.com>
2025-04-27 16:21:29 -0700

    cool stuff

---
+++ Breakout Ball Game/breakout.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canavas.getElementById("2d")
+
+let paddleX = 200;
+
+function drawPaddle(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(200,300,80,10);
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft"){
+        paddleX -= 20;
+    }
+    else if (e.key === "ArrowRight"){
+        paddleX += 20;
+    }
+    ctx.clearRect(0,0,canvas.replaceChild.width,canvas.height)
+    drawPaddle()
+})
